34034a202f9b185d49035e64233acc3105c7f28b
MoisesPiguave <moi.emanuelps@gmail.com>
2025-04-22 17:46:18 +0100

    ahora si ya vale

--- src/App.java
+++ src/App.java
@@ -1,14 +1,54 @@
-
+import Controllers.PersonaController;
 import Models.Persona;
+import views.ViewConsole;
 
 public class App {
     public static void main(String[] args) throws Exception {
         Persona[] personas = new Persona[] {
+            new Persona("Juan", 25),
+            new Persona("Maria", 30),
+            new Persona("Carlos", 22),
+            new Persona("Ana", 28),
+            new Persona("Luis", 35),
+            new Persona("Sofia", 27),
+            new Persona("Miguel", 40),
+            new Persona("Laura", 32),
+            new Persona("Pedro", 29),
+            new Persona("Elena", 26)
+
+
 
         };
 
-        /// TODO: GENERAR INSTNACIA DE CONTROLLE Y ORDENAR EL METODO DESPUES BUSCAR POR
-        /// LA EDAD
+        ViewConsole VC = new ViewConsole();
+        PersonaController PC = new PersonaController();
+    
+        VC.printMessage("Personas");
+        VC.printPersonsArray(personas);
+        PC.ordenarPorEdad(personas);
+        System.out.print("\n");
+        VC.printMessage("Arreglo Ordenado por Edad");
+        VC.printPersonsArray(personas);
+        Persona p = PC.buscarPorEdad(personas, 40);
+        if (p == null){
+            VC.printMessage("No existe una persona con esa edad");
+        }else{
+            VC.printMessage("\n\tLa persona con la edad 40 es: ");
+            VC.printMessage("\t" + p);
+        }
+        Persona pe = PC.buscarPorEdad(personas, 99);
+        if (pe == null){
+            VC.printMessage("No existe una persona con esa edad");
+        }else{
+            VC.printMessage("\tLa persona con la edad 40 es: ");
+            VC.printMessage("\t" + pe);
+        }
+
+        
+
+
+       
 
     }
+    
 }
---
+++ src/views/ViewConsole.java
@@ -0,0 +1,22 @@
+package views;
+import Models.Persona;
+
+public class ViewConsole {
+
+    public void printPersonsArray(Persona[] personas) {
+
+        for ( int i = 0 ; i<personas.length ; i++){
+
+            System.out.print("|| " + personas[i] + " || ");
+        }    
+    }
+    public void printMessage(String message){
+        System.out.println(message);
+    }
+
+
+
+
+
+    
+}
